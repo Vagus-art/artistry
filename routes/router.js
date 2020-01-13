41e79e289c624599a865f2419d671d2b01131456
Vagus-art <agustin.favo@gmail.com>
2020-01-13 13:52:20 -0300

    login system works

--- routes/router.js
+++ routes/router.js
@@ -1,29 +1,81 @@
 const express = require('express');
 const router = express.Router();
 const postModel = require('../models/postModel');
+const userModel = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+
+//check if session was started, pass as variable to every render please
+let check = ()=>(sess.nickname!=undefined);
+
 
 //get
 router.get('/',(req,res)=>{
-  res.render('home');
+  sess = req.session;
+  res.render('home',{message:'Welcome to artistry! Log in or Sign up to show your talents.',nickname:sess.nickname,chcksess:check()});
 });
 router.get('/search', async (req,res)=>{
   try{
+    sess = req.session;
     //search for partial string
     myregex = new RegExp(req.query.searchbar,"i");
     const result = await postModel.find({title:{ $regex: myregex, $options: 'i' }}).lean();
     if(result.length!=0){
-      res.render('search',{results:result});
+      res.render('search',{results:result,chcksess:check()});
     }
     else{
-      res.render('search',{message:'No results found for \"' + req.query.searchbar + "\""});
+      res.render('search',{message:'No results found for \"' + req.query.searchbar + "\"",chcksess:check()});
     }
   }
   catch(err){
-    console.log(err);
+    console.log('there has been an error on the search route: ' + err);
   }
+});
+router.get('/login',(req,res)=>{
+  res.render('login');
+});
+router.get('/signup',(req,res)=>{
+  res.render('signup');
+});
+router.get('/logout',(req,res)=>{
+  req.session.destroy();
+  res.redirect('/');
 })
 
-//api
+//post
+router.post('/signup',async (req,res)=>{
+  const user = new userModel({
+    nickname:req.body.nickname,
+    password:await bcrypt.hash(req.body.password,10),
+    email:req.body.email
+  });
+  await user.save();
+  res.render('Home',{message:'User ' + req.body.nickname + ' has been registered succesfully'});
+});
+router.post('/login',async (req,res)=>{
+  try{
+    sess = req.session;
+    const [nickname,password] = [req.body.nickname,req.body.password];
+    const user = await userModel.findOne({nickname:nickname});
+    if(user){
+        const match = await bcrypt.compare(password,user.password);
+        if(match){
+        sess.nickname = nickname;
+        res.redirect('/');
+        }
+        else{
+        res.render('login',{message:"Password invalid!"});
+        }
+    }
+    else{
+        res.render('login',{message:'Nickname not found!'})
+    }
+}
+catch(error){
+  console.log(error,'login');
+}
+});
+
+//dev api
 router.get('/api', async (req,res)=>{
   try {const db = await postModel.find();
   res.json(db);}
@@ -41,5 +93,9 @@ router.post('/api', async (req,res)=>{
   await post.save();
   res.send('done');
 });
+router.get('/api/users',async (req,res)=>{
+  const users = await userModel.find();
+  res.json(users);
+})
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require('express');
-const app = express();
+const app = module.exports = express();
 const exphbs = require('express-handlebars');
 const mongoose = require('mongoose');
 const database = require('./database.js');
 const port = process.env.PORT || 3000;
 const router = require('./routes/router.js');
 const bodyparser = require('body-parser');
+const session = require('express-session');
+const uuid = require('uuid');
 
+var sess;
 
 //handlebars
 app.engine('handlebars', exphbs());
@@ -14,10 +17,19 @@ app.set('view engine', 'handlebars');
 
 //middleware
 app.use(express.static('./public'));
-app.use(bodyparser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyparser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyparser.json())
+// sessions
+app.use(session({
+    genid: ()=>uuid(),
+  secret: 'mysecret'})
+);
 
 //routes
 app.use('/', router);
+
 
 //server start
 app.listen(port,()=>console.log(`Server listening on port ${port}`))
