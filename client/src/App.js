5ea881716e79638bf2db956d4b058259056c7ca1
Vagus-art <agustin.favo@gmail.com>
2020-01-18 11:38:14 -0300

    connected profile tab to store

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Profile from './components/Profile';
 import Settings from './components/Settings';
 import NavBar from './components/NavBar';
 import Home from './components/Home';
+import { connect } from 'react-redux';
 
+const mapStateToProps = state => ({
+  user: state.user
+})
 
-function App() {
+function App(props) {
   return (
     <div className="App">
       <Router>
             <NavBar />
             <Switch>
               <Route exact path='/' component={Home} />
-              <Route path='/profile' component={Profile} />
+              <Route path='/profile' render={()=><Profile user={props.user} />} />
               <Route path='/settings' component={Settings} />
             </Switch>
           </Router>
@@ -22,4 +26,4 @@ function App() {
   )
 }
 
-export default App;
+export default connect(mapStateToProps,()=>({}))(App);
--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,8 @@ import { Provider } from 'react-redux';
 import { promiseMiddleware } from './middleware';
 
 const initialState = {
-    posts:null
+    posts:null,
+    user:'guest'
 }
 
 const reducer = (state = initialState, action) => {
